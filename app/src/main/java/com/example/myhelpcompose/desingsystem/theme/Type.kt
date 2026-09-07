9f18200ccdfbb10cf7fd77e9d07c3e0354a00fb1
ricardosr4 <ricardosr4@users.noreply.github.com>
2026-09-06 23:29:33 -0300

    primer commit

--- app/src/main/java/com/example/myhelpcompose/desingsystem/theme/Type.kt
+++ app/src/main/java/com/example/myhelpcompose/desingsystem/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.myhelpcompose.ui.theme
+package com.example.myhelpcompose.desingsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
